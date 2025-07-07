a9ad9744e7c8df2438ea776229fe019f76e650c5
Faiza <berrichif0717@gmail.com>
2025-07-07 11:03:58 +0200

    initializing projects

---
+++ cv-service/src/main/java/com/example/cv_service/CvServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cv_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvServiceApplication.class, args);
+	}
+
+}
