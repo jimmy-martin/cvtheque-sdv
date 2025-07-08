05f466a85d412676b12ccdcb11fbae654fa5a853
Faiza <berrichif0717@gmail.com>
2025-07-08 11:24:27 +0200

    add compinent register and login

--- cv-app-front/src/app/app.module.ts
+++ cv-app-front/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
@@ -23,4 +33,4 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
