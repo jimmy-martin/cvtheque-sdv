7fbed62071629dd5b3925ba5aaccf46d40ee7d05
Prajay-Chaudhary <prajaychaudhary5678@gmail.com>
2025-07-08 16:05:50 +0200

    feat: upload cv

--- cv-service/src/main/java/com/example/cv_service/controller/CvController.java
+++ cv-service/src/main/java/com/example/cv_service/controller/CvController.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/cvs")
 public class CvController {
