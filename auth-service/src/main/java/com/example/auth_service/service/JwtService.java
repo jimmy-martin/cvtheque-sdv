9c94a254e2b53a48b32802d03494b74d11304daa
Faiza <berrichif0717@gmail.com>
2025-07-08 16:30:26 +0200

    feature/profile: add profile entity

--- auth-service/src/main/java/com/example/auth_service/service/JwtService.java
+++ auth-service/src/main/java/com/example/auth_service/service/JwtService.java
@@ -1,12 +1,16 @@
 package com.example.auth_service.service;
 
+import com.example.auth_service.entity.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import com.example.auth_service.entity.Role;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
@@ -17,9 +21,14 @@ public class JwtService {
     @Value("${jwt.expiration}")
     private long expirationTime;
 
-    public String generateToken(String email) {
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", user.getId());
+        claims.put("roles", user.getRoles().stream().map(Role::getName).toList());
+
         return Jwts.builder()
-                .setSubject(email)
+                .setClaims(claims)
+                .setSubject(user.getEmail())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(getSigningKey())
