9a519b7e8218f623497e0d0871f3f8d23dc81c44
Prajay-Chaudhary <prajaychaudhary5678@gmail.com>
2025-07-09 10:06:21 +0200

    refactor frontend testing specs files

--- cv-app-front/src/app/auth/login/login.component.spec.ts
+++ cv-app-front/src/app/auth/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
+import { AuthService } from 'src/app/service/auth/auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +10,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [ReactiveFormsModule, HttpClientTestingModule],
+      declarations: [LoginComponent],
+      providers: [AuthService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
--- cv-app-front/src/app/auth/register/register.component.spec.ts
+++ cv-app-front/src/app/auth/register/register.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterComponent } from './register.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthService } from 'src/app/service/auth/auth.service';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,9 +9,10 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterComponent],
+      imports: [HttpClientTestingModule],
+      providers: [AuthService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
